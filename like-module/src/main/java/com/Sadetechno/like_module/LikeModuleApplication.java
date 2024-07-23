4579a20bff00b74bbbb0d31b812acfcce86bc7a2
Suresh-sade <msgmanagementservice@gmail.com>
2024-07-24 02:14:25 +0530

    like module

---
+++ like-module/src/main/java/com/Sadetechno/like_module/LikeModuleApplication.java
@@ -0,0 +1,12 @@
+package com.Sadetechno.like_module;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeModuleApplication.class, args);
+	}
+
+}
---
+++ like-module/src/test/java/com/Sadetechno/like_module/LikeModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sadetechno.like_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
