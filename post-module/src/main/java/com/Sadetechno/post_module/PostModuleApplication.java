5be7230435cdb1e6a034398c99422f81d801dbd1
Suresh-sade <msgmanagementservice@gmail.com>
2024-07-23 16:02:27 +0530

    post module

---
+++ post-module/src/main/java/com/Sadetechno/post_module/PostModuleApplication.java
@@ -0,0 +1,13 @@
+package com.Sadetechno.post_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostModuleApplication.class, args);
+	}
+
+}
---
+++ post-module/src/test/java/com/Sadetechno/post_module/PostModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sadetechno.post_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
